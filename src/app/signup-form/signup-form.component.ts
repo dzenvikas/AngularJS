0ff520e635e03a1e845c0efce6ce136d1e30b40b
dzenvikas <vikaskumar.vsk@gmail.com>
2018-10-28 22:41:26 +0530

    custom validator

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -1,19 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-
+import { UsernameValidators } from '../common/validators/username.validators';
 @Component({
   selector: 'app-signup-form',
   templateUrl: './signup-form.component.html',
   styleUrls: ['./signup-form.component.scss']
 })
-export class SignupFormComponent implements OnInit {
+export class SignupFormComponent {
 
   form = new FormGroup({
     username: new FormControl('', [
       Validators.required,
-      Validators.minLength(3)
+      Validators.minLength(3),
+      UsernameValidators.cannotContainSpace,
+      UsernameValidators.shouldBeUnique
     ]),
-    password: new FormControl('', Validators.required)
+    password: new FormControl('',
+      Validators.required)
   });
 
   get username() {
